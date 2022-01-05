4d7d156df917d531d006d92cab1cae0363e59953
kohuro <sharf.toto.fly@gmail.com>
2022-01-05 23:08:07 +0300

    Аккордеоны FAQ

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -6,7 +6,19 @@ $(document).ready(function() {
     $('.js-main-nav').toggleClass('main-nav-open');
   });
 
+  // Аккордеоны для FAQ
+  let prevBtn;
+  $('.js-accordeon-btn').on('click', function(){
 
+    if (prevBtn === $(this)[0]) {
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $('.js-accordeon-btn').next().slideUp();
+    $(this).next().slideDown();
+    prevBtn = $(this)[0];
+  })
 
 
  });
