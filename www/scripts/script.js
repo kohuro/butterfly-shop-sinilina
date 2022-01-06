9dc893c6b32883b7e88b5aab7daec7a5aab54492
kohuro <sharf.toto.fly@gmail.com>
2022-01-06 16:50:17 +0300

    Страница контактов, табы

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -6,18 +6,49 @@ $(document).ready(function() {
     $('.js-main-nav').toggleClass('main-nav-open');
   });
 
+  // Табы для контактов
+    $('.tabs-link').on('click', function(e){
+
+      e.preventDefault();
+
+      let index = $(this).index('.tabs-link');
+
+      $('.tabs-link').removeClass('active');
+      $(this).addClass('active');
+
+      $('.contacts-content').removeClass('active');
+      $('.contacts-content').eq(index).addClass('active');
+    })
+
   // Аккордеоны для FAQ
   let prevBtn;
-  $('.js-accordeon-btn').on('click', function(){
+    $('.js-accordeon-btn').on('click', function(){
 
     if (prevBtn === $(this)[0]) {
       $(this).next().slideToggle();
+      // $(this).find('.js-faq-icon').css({'background-image': 'url(/images/i-plus.svg)'});
       return;
     }
 
     $('.js-accordeon-btn').next().slideUp();
+    // $('.js-accordeon-btn').find('.js-faq-icon').css({'background-image': 'url(/images/i-plus.svg)'});
     $(this).next().slideDown();
+    // $(this).find('.js-faq-icon').css({'background-image': 'url(/images/i-minus.svg)'});
+
     prevBtn = $(this)[0];
+  })
+
+  let openIcon = false;
+  $('.js-accordeon-btn').on('click', function(){
+
+    if (openIcon === false) {
+      $(this).find('.js-faq-icon').css({'background-image': 'url(/images/i-minus.svg)'});
+      openIcon = true;
+      return;
+    }
+
+    $('.js-accordeon-btn').find('.js-faq-icon').css({'background-image': 'url(/images/i-plus.svg)'});
+    openIcon = false;
   })
 
 
